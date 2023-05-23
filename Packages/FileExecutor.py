de846b5f667cde9f6317439bc026fbbecae164fc
fostn <106077105+fostn@users.noreply.github.com>
2023-05-23 19:08:34 +0300

    Update FileExecutor.py

--- Packages/FileExecutor.py
+++ Packages/FileExecutor.py
@@ -1,20 +1,19 @@
 import os
 import subprocess
-
-
 class FileExecutor:
     @staticmethod
-    def get_execution_command(file_path):
+    def get_execution_command(file_path, folder_path):
         _, file_extension = os.path.splitext(file_path)
         if file_extension == ".py":
-            return f"python3 {file_path}"
+            return f"python3 {file_path} {folder_path}"
         elif file_extension == ".java":
-            return f"java {file_path}"
+            return f"java {file_path} {folder_path}"
         elif file_extension == ".c":
-            return f"gcc {file_path} -o {file_path}.exe && {file_path}.exe"
+            return f"gcc {file_path} -o {os.path.join(folder_path, file_path)}.exe && {os.path.join(folder_path, file_path)}.exe"
         elif file_extension == ".cpp":
-            return f"g++ {file_path} -o {file_path}.exe && {file_path}.exe"
+            return f"g++ {file_path} -o {os.path.join(folder_path, file_path)}.exe && {os.path.join(folder_path, file_path)}.exe"
         elif file_extension == ".cs":
-            return f"dotnet run --project {file_path}"
+            return f"dotnet run --project {file_path} {folder_path}"
         else:
             return None
+
