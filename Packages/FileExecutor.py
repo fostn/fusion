4807e725879f3bcc094d5f1513d9ca76ba6d9c70
fostn <106077105+fostn@users.noreply.github.com>
2023-05-24 10:48:59 +0300

    Update FileExecutor.py

--- Packages/FileExecutor.py
+++ Packages/FileExecutor.py
@@ -1,19 +1,32 @@
 import os
-import subprocess
+import pexpect
+
+
 class FileExecutor:
     @staticmethod
-    def get_execution_command(file_path, folder_path):
+    def get_execution_command(file_path, target_folder):
         _, file_extension = os.path.splitext(file_path)
         if file_extension == ".py":
-            return f"python3 {file_path} {folder_path}"
+            return f"python3 {file_path} {target_folder}"
         elif file_extension == ".java":
-            return f"java {file_path} {folder_path}"
+            return f"java {file_path} {target_folder}"
         elif file_extension == ".c":
-            return f"gcc {file_path} -o {os.path.join(folder_path, file_path)}.exe && {os.path.join(folder_path, file_path)}.exe"
+            return f"gcc {file_path} -o {os.path.join(target_folder, file_path)}.exe && {os.path.join(target_folder, file_path)}.exe"
         elif file_extension == ".cpp":
-            return f"g++ {file_path} -o {os.path.join(folder_path, file_path)}.exe && {os.path.join(folder_path, file_path)}.exe"
+            return f"g++ {file_path} -o {os.path.join(target_folder, file_path)}.exe && {os.path.join(target_folder, file_path)}.exe"
         elif file_extension == ".cs":
-            return f"dotnet run --project {file_path} {folder_path}"
+            return f"dotnet run --project {file_path} {target_folder}"
         else:
             return None
+
+    @staticmethod
+    def execute_file(file_path, target_folder):
+        execution_command = FileExecutor.get_execution_command(file_path, target_folder)
+        if execution_command:
+            child = pexpect.spawn(execution_command)
+            child.interact()
+        else:
+            print("Invalid file extension.")
+
+
 
